2ea0fdb048dc67232b76e9392941e828e2e81c94
Tech VJ <122880036+VJBots@users.noreply.github.com>
2024-07-20 20:09:54 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,10 +1,10 @@
 import os
 from os import environ
 
-API_ID       = int(environ.get("API_ID", ""))
-API_HASH     = environ.get("API_HASH", "")
-BOT_TOKEN    = environ.get("BOT_TOKEN", "")
-DATABASE_URI = environ.get("DATABASE_URI", "")
-LOG_CHANNEL  = int(environ.get("LOG_CHANNEL", ""))
-ADMIN        = int(environ.get("ADMIN", ""))
+API_ID       = int(environ.get("API_ID", "22182189"))
+API_HASH     = environ.get("API_HASH", "5e7c4088f8e23d0ab61e29ae11960bf5")
+BOT_TOKEN    = environ.get("BOT_TOKEN", "6627318053:AAE4jkJc0qXkD81K-rX1GJrZqksUg7Iv0mY")
+DATABASE_URI = environ.get("DATABASE_URI", "mongodb+srv://mihaja5084:yeIh95RrMkRNZ3It@cluster0.6voc3fm.mongodb.net/?retryWrites=true&w=majority")
+LOG_CHANNEL  = int(environ.get("LOG_CHANNEL", "-1002131110143"))
+ADMIN        = int(environ.get("ADMIN", "6073523936"))
 CHANNEL      = environ.get("CHANNEL", "@VJ_Botz")
