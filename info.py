10a35c92117b8edfda2a0427e752947a61c9bad1
Tech VJ <122880036+VJBots@users.noreply.github.com>
2024-07-21 14:18:59 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,10 +1,16 @@
+# Don't Remove Credit Tg - @VJ_Botz
+# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
+# Ask Doubt on telegram @KingVJ01
+
+# Never Give DATABASE_URI And BOT_TOKEN Here If Repo Is Forked Or Public Because This May Lead Account Ban.
+
 import os
 from os import environ
 
-API_ID       = int(environ.get("API_ID", "22182189"))
-API_HASH     = environ.get("API_HASH", "5e7c4088f8e23d0ab61e29ae11960bf5")
-BOT_TOKEN    = environ.get("BOT_TOKEN", "6627318053:AAHFeyNF255TOFm4jtyeiAAcqOukvIHyN-Q")
-DATABASE_URI = environ.get("DATABASE_URI", "mongodb+srv://mihaja5084:yeIh95RrMkRNZ3It@cluster0.6voc3fm.mongodb.net/?retryWrites=true&w=majority")
-LOG_CHANNEL  = int(environ.get("LOG_CHANNEL", "-1002131110143"))
-ADMIN        = int(environ.get("ADMIN", "6073523936"))
+API_ID       = int(environ.get("API_ID", ""))
+API_HASH     = environ.get("API_HASH", "")
+BOT_TOKEN    = environ.get("BOT_TOKEN", "")
+DATABASE_URI = environ.get("DATABASE_URI", "")
+LOG_CHANNEL  = int(environ.get("LOG_CHANNEL", ""))
+ADMIN        = int(environ.get("ADMIN", ""))
 CHANNEL      = environ.get("CHANNEL", "@VJ_Botz")
