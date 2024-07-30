2ab72c1e753bfdd3ddac33e77b6813fce1c2748b
Luck Mix <153753933+lk3140318@users.noreply.github.com>
2024-07-30 10:28:08 +0530

    Update info.py

--- info.py
+++ info.py
@@ -7,10 +7,10 @@
 import os
 from os import environ
 
-API_ID       = int(environ.get("API_ID", ""))
-API_HASH     = environ.get("API_HASH", "")
+API_ID       = int(environ.get("API_ID", "24335028"))
+API_HASH     = environ.get("API_HASH", "b204ec833fb451fb913fc8e683b232d0")
 BOT_TOKEN    = environ.get("BOT_TOKEN", "")
 DATABASE_URI = environ.get("DATABASE_URI", "")
-LOG_CHANNEL  = int(environ.get("LOG_CHANNEL", ""))
-ADMIN        = int(environ.get("ADMIN", ""))
+LOG_CHANNEL  = int(environ.get("LOG_CHANNEL", "-1002045377846"))
+ADMIN        = int(environ.get("ADMIN", "5213073489"))
 CHANNEL      = environ.get("CHANNEL", "@VJ_Botz")
